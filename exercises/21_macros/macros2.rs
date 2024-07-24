428ab5873f45cf92d1fc51893e3ee7418b65cc31
Micro-ATP <131521928+Micro-ATP@users.noreply.github.com>
2024-07-24 16:25:36 +0800

    macros

--- exercises/21_macros/macros2.rs
+++ exercises/21_macros/macros2.rs
@@ -1,10 +1,21 @@
-fn main() {
-    my_macro!();
-}
+// fn main() {
+//     my_macro!();
+// }
 
-// TODO: Fix the compiler error by moving the whole definition of this macro.
+// // TODO: Fix the compiler error by moving the whole definition of this macro.
+// macro_rules! my_macro {
+//     () => {
+//         println!("Check out my macro!");
+//     };
+// }
+
+// 将宏定义移到 `main` 函数之前
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
     };
+}
+
+fn main() {
+    my_macro!();
 }
